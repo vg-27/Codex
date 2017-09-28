42f360ff8acf7745abd25cd39287759acfdd9c26
sahilsaiyad <sahilsaiyad90@gmail.com>
2017-09-28 20:38:32 +0530

    Login Interface(Django)

---
+++ userauth/log/views.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+@login_required(login_url="login/")
+def home(request):
+	return render(request,"home.html")
